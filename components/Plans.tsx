92238e7addd65f0f68be49695f123632b00311d7
Adrian Domański <adrian.domanski@prographers.com>
2023-03-02 09:52:09 +0100

    Fix plans page not beeing in the center

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -1,82 +1,83 @@
-import { Product } from '@stripe/firestore-stripe-payments';
-import Head from 'next/head';
-import Link from 'next/link';
-import { useState } from 'react';
-import { FaCheck } from 'react-icons/fa';
-import useAuth from '../hooks/useAuth';
-import { loadCheckout } from '../lib/stripe';
-import Loader from './Loader';
-import Table from './Table';
+import { Product } from '@stripe/firestore-stripe-payments'
+import Head from 'next/head'
+import Link from 'next/link'
+import { useState } from 'react'
+import useAuth from '../hooks/useAuth'
+import { loadCheckout } from '../lib/stripe'
+import Table from './Table'
+import Loader from './Loader'
+import { FaCheck } from 'react-icons/fa'
 
-interface IProps {
-  products: Product[];
+interface Props {
+  products: Product[]
 }
 
-function Plans({ products }: IProps) {
-  const { logout, user } = useAuth();
-  const [selectedPlan, setSelectedPlan] = useState<Product | null>(products[2]);
-  const [isBillingLoading, setIsBillingLoading] = useState(false);
+function Plans({ products }: Props) {
+  const { logout, user } = useAuth()
+  const [selectedPlan, setSelectedPlan] = useState<Product | null>(products[2])
+  const [isBillingLoading, setBillingLoading] = useState(false)
+
+  console.log(products)
 
   const subscribeToPlan = () => {
-    if (!user) return;
+    if (!user) return
 
-    loadCheckout(selectedPlan?.prices[0].id!);
-    setIsBillingLoading(true);
-  };
+    loadCheckout(selectedPlan?.prices[0].id!)
+    setBillingLoading(true)
+  }
 
   return (
     <div>
       <Head>
         <title>Netflix</title>
-        <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <header className='border-b border-white/10 bg-[#141414]'>
-        <Link href='/'>
+      <header className="border-b border-white/10 bg-[#141414]">
+        <Link href="/">
           <img
-            src='https://rb.gy/ulxxee'
-            alt='Netflix'
+            src="https://rb.gy/ulxxee"
+            alt="Netflix"
             width={150}
             height={90}
-            className='cursor-pointer object-contain'
+            className="cursor-pointer object-contain"
           />
         </Link>
         <button
-          className='text-lg font-medium hover:underline'
+          className="text-lg font-medium hover:underline"
           onClick={logout}
         >
           Sign Out
         </button>
       </header>
 
-      <main className='max-w-5xl pt-28 pb-12 transition-all md:px-10'>
-        <h1 className='mb-3 text-3xl font-medium'>
+      <main className="mx-auto max-w-5xl px-5 pt-28 pb-12 transition-all md:px-10">
+        <h1 className="mb-3 text-3xl font-medium">
           Choose the plan that's right for you
         </h1>
         <ul>
-          <li className='flex items-center gap-x-2 text-lg'>
-            <FaCheck className='h-7 w-7 text-[#E50914]' /> Watch all you want.
+          <li className="flex items-center gap-x-2 text-lg">
+            <FaCheck className="h-7 w-7 text-[#E50914]" /> Watch all you want.
             Ad-free.
           </li>
-          <li className='flex items-center gap-x-2 text-lg'>
-            <FaCheck className='h-7 w-7 text-[#E50914]' /> Recommendations just
-            for you.
+          <li className="flex items-center gap-x-2 text-lg">
+            <FaCheck className="h-7 w-7 text-[#E50914]" /> Recommendations
+            just for you.
           </li>
-          <li className='flex items-center gap-x-2 text-lg'>
-            <FaCheck className='h-7 w-7 text-[#E50914]' /> Change or cancel your
-            plan anytime.
+          <li className="flex items-center gap-x-2 text-lg">
+            <FaCheck className="h-7 w-7 text-[#E50914]" /> Change or cancel
+            your plan anytime.
           </li>
         </ul>
 
-        <div className='mt-4 flex flex-col space-y-4'>
-          <div className='flex w-full items-center self-end md:w-3/5'>
+        <div className="mt-4 flex flex-col space-y-4">
+          <div className="flex w-full items-center justify-end self-end md:w-3/5">
             {products.map((product) => (
               <div
-                className={`planBox cursor-pointer transition ${
+                className={`planBox ${
                   selectedPlan?.id === product.id ? 'opacity-100' : 'opacity-60'
                 }`}
-                onClick={() => setSelectedPlan(product)}
                 key={product.id}
+                onClick={() => setSelectedPlan(product)}
               >
                 {product.name}
               </div>
@@ -93,7 +94,7 @@ function Plans({ products }: IProps) {
             onClick={subscribeToPlan}
           >
             {isBillingLoading ? (
-              <Loader color='dark:fill-gray-300' />
+              <Loader color="dark:fill-gray-300" />
             ) : (
               'Subscribe'
             )}
@@ -101,7 +102,7 @@ function Plans({ products }: IProps) {
         </div>
       </main>
     </div>
-  );
+  )
 }
 
-export default Plans;
+export default Plans
